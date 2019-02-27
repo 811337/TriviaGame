de2b53a5410af9f99e11bf8db5108c6f585f252d
Jesse Cheng <811337@gmail.com>
2019-02-26 22:36:25 -0600

    update1

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,39 @@
+//only show the intro on load
 window.onload = function() {
     document.getElementById("intro").hidden = false;
     document.getElementById("question").hidden = true;
-}
+    document.getElementById("end").hidden = true;
+};
 
-var number = 60;
-var intervalId;
+var number = 600;
+var count;
 
 function decrement() {
     number--;
     $("#seconds").html("<span>" + number + "</span>");
-}
+
+    //stops the timer and goes to results
+    if (number === 0) {
+        clearInterval(count)
+        document.getElementById("question").hidden = true;
+        document.getElementById("end").hidden = false;
+    };
+};
 
 function timer() {
-    intervalId = setInterval(decrement, 1000);
-}
+    count = setInterval(decrement, 1000);
+};
 
+//once begin button is clicked, changes to questions and begins timer
 document.getElementById("startButton").addEventListener("click", function() {
     document.getElementById("intro").hidden = true;
     document.getElementById("question").hidden = false;
     timer();
-}, false);
+});
+
+
+//once submit button is clicked, changes to results
+document.getElementById("endButton").addEventListener("click", function() {
+    document.getElementById("question").hidden = true;
+    document.getElementById("end").hidden = false;
+});
